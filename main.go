153f4b4b79ddbc0f5cc723e0336d793b4dcb9273
Sven Anderson <sven@anderson.de>
2023-04-06 00:11:34 +0200

    Fix ejson handling

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/sideshow/apns2"
@@ -20,31 +18,19 @@ import (
 type RCPushNotification struct {
 	Token   string `json:"token"`
 	Options struct {
-		CreatedAt string `json:"createdAt"`
-		CreatedBy string `json:"createdBy"`
-		Sent      bool   `json:"sent"`
-		Sending   int    `json:"sending"`
-		From      string `json:"from"`
-		Title     string `json:"title"`
-		Text      string `json:"text"`
-		UserId    string `json:"userId"`
-		Payload   struct {
-			Host             string `json:"host"`
-			MessageId        string `json:"messageId"`
-			NotificationType string `json:"notificationType"`
-			Rid              string `json:"rid"`
-			Sender           struct {
-				Id       string `json:"_id"`
-				Username string `json:"username"`
-				Name     string `json:"name"`
-			} `json:"sender"`
-			SenderName string `json:"senderName"`
-			Type       string `json:"type"`
-		} `json:"payload"`
-		Badge int    `json:"badge"`
-		Sound string `json:"sound"`
-		NotId int    `json:"notId"`
-		Apn   struct {
+		CreatedAt string      `json:"createdAt"`
+		CreatedBy string      `json:"createdBy"`
+		Sent      bool        `json:"sent"`
+		Sending   int         `json:"sending"`
+		From      string      `json:"from"`
+		Title     string      `json:"title"`
+		Text      string      `json:"text"`
+		UserId    string      `json:"userId"`
+		Payload   interface{} `json:"payload"`
+		Badge     int         `json:"badge"`
+		Sound     string      `json:"sound"`
+		NotId     int         `json:"notId"`
+		Apn       struct {
 			Category string `json:"category"`
 			Text     string `json:"text"`
 		} `json:"apn"`
@@ -57,6 +43,20 @@ type RCPushNotification struct {
 	} `json:"options"`
 }
 
+type RCPayload struct {
+	Host             string `json:"host"`
+	MessageId        string `json:"messageId"`
+	NotificationType string `json:"notificationType"`
+	Rid              string `json:"rid"`
+	Sender           struct {
+		Id       string `json:"_id"`
+		Username string `json:"username"`
+		Name     string `json:"name"`
+	} `json:"sender"`
+	SenderName string `json:"senderName"`
+	Type       string `json:"type"`
+}
+
 func main() {
 	cert, err := certificate.FromP12File(
 		os.Getenv("RCPG_APNS_CERT_FILE"),
@@ -65,7 +65,8 @@ func main() {
 	if err != nil {
 		log.Fatal("Cert Error:", err)
 	}
-	client := apns2.NewClient(cert).Development()
+	//client := apns2.NewClient(cert).Development()
+	client := apns2.NewClient(cert).Production()
 
 	infoHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Rocket.Chat Push Gateway\n")
@@ -109,15 +110,22 @@ func getAPNPushNotificationHandler(client *apns2.Client) func(http.ResponseWrite
 		}
 
 		opt := &notification.Options
+
+		ejson, _ := json.Marshal(opt.Payload)
+
+		var rcPayload RCPayload
+		err = json.Unmarshal(ejson, &rcPayload)
+		if err != nil {
+			log.Println("Failed to parse request payload: ", err)
+		}
+
 		// Create the notification payload
 		p := payload.NewPayload().
 			AlertTitle(opt.Title).
 			AlertBody(opt.Text).
 			Badge(opt.Badge).
 			Sound(opt.Sound).
-			MutableContent().
-			ContentAvailable().
-			Custom("ejson", opt.Payload)
+			Custom("ejson", string(ejson))
 
 		if opt.Apn.Category != "" {
 			p.Category(opt.Apn.Category)
@@ -127,12 +135,20 @@ func getAPNPushNotificationHandler(client *apns2.Client) func(http.ResponseWrite
 			p.AlertBody(opt.Apn.Text)
 		}
 
+		if rcPayload.NotificationType == "message-id-only" {
+			p.MutableContent()
+			p.ContentAvailable()
+		}
+
 		// Create the notification
 		n := &apns2.Notification{
 			DeviceToken: notification.Token,
 			Topic:       opt.Topic,
 			Payload:     p,
 		}
+
+		nJSON, _ := n.MarshalJSON()
+		log.Println("Sending notification: ", string(nJSON))
 
 		// Send the notification
 		res, err := client.Push(n)
@@ -154,45 +170,4 @@ func getAPNPushNotificationHandler(client *apns2.Client) func(http.ResponseWrite
 		// Send a success response
 		w.WriteHeader(http.StatusOK)
 	}
-}
-
-func handlePushNotification2(w http.ResponseWriter, r *http.Request, client apns2.Client) {
-	// Read the request body
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Parse the request body as JSON
-	var pushData RCPushNotification
-	if err := json.Unmarshal(body, &pushData); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Create a new notification with the given payload and options
-	payload := &payload.Payload{}
-
-	notification := &apns2.Notification{
-		DeviceToken: pushData.Token,
-		Payload:     payload,
-		Priority:    apns2.PriorityHigh,
-		Expiration:  time.Now().Add(time.Hour * 1),
-		ApnsID:      fmt.Sprintf("%d", pushData.Options.NotId),
-		Topic:       pushData.Options.Topic,
-		CollapseID:  pushData.Options.UniqueId,
-	}
-
-	// Send the push notification using the APNs client
-	res, err := client.Push(notification)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Return the response from the APNs server
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(res)
 }
